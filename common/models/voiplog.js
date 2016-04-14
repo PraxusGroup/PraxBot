d588d0c3ac2e9097736369c3f81fd040df7f3c58
Whiplash88 <whiplash174@gmail.com>
2016-04-14 09:11:54 -0400

    random

--- common/models/voiplog.js
+++ common/models/voiplog.js
@@ -1,3 +1,32 @@
 module.exports = function(Voiplog) {
 
+//cb(null, response.length.toString());
+    
+  // Render all activity logs into an array for the chart
+  Voiplog.getAllActivityData = function(cb) {
+    var response = Gamer.find({
+        where: {
+          lastForumVisit: {
+            gt: '2000-01-01'
+          }
+        }
+      })
+      .then(function(response) {
+        
+      });
+  };
+
+  Gamer.remoteMethod(
+    'getAllActivityData', {
+      http: {
+        path: '/getallactivitydata',
+        verb: 'get'
+      },
+      returns: {
+        arg: 'count',
+        type: 'string'
+      }
+    }
+  );
+
 };
